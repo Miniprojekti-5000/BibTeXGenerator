8e8cb604577296d401b5030620759eaf4aba601e
avrj <anraiha@cs.helsinki.fi>
2015-09-26 20:28:43 +0300

    luotu maven-projektin runko

---
+++ src/main/java/ohtu/miniprojekti5000/App.java
@@ -0,0 +1,13 @@
+package ohtu.miniprojekti5000;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
