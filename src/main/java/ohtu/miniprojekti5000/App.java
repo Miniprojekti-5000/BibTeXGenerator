a86faf64868e500ca906aaf6170e533cc8c16d36
avrj <anraiha@cs.helsinki.fi>
2015-09-28 21:30:50 +0300

    lisätty alustava käyttöliittymä ja käyttöliittymän käynnistäminen pääohjelmasta

--- src/main/java/ohtu/miniprojekti5000/App.java
+++ src/main/java/ohtu/miniprojekti5000/App.java
@@ -1,5 +1,8 @@
 package ohtu.miniprojekti5000;
 
+import ohtu.miniprojekti5000.logic.References;
+import ohtu.miniprojekti5000.ui.UI;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        References references = new References();
+
+        UI ui = new UI(references);
+
+        ui.run();
     }
 }
---
+++ src/main/java/ohtu/miniprojekti5000/ui/UI.java
@@ -0,0 +1,71 @@
+package ohtu.miniprojekti5000.ui;
+
+import ohtu.miniprojekti5000.logic.BookReference;
+import ohtu.miniprojekti5000.logic.ReferenceInterface;
+import ohtu.miniprojekti5000.logic.References;
+
+import java.util.Scanner;
+
+/**
+ * Created by avrj on 28.9.2015.
+ */
+
+
+public class UI {
+    private References references;
+
+    public UI(References references) {
+        this.references = references;
+    }
+
+    public void run() {
+        boolean addNewEntry = true;
+
+        Scanner user_input = new Scanner(System.in);
+
+        while (addNewEntry) {
+            System.out.println("What type of reference?");
+            System.out.println("1) Book");
+            System.out.println("");
+
+            System.out.print("Make your selection and hit ENTER: ");
+
+            if (user_input.next().equals("1")) {
+                BookReference bookReference = new BookReference();
+
+                System.out.println("Add new \"Book\" entry");
+                System.out.println("");
+
+                System.out.print("Heading:");
+                bookReference.setHeading(user_input.next());
+
+                System.out.print("Author:");
+                bookReference.setAuthor(user_input.next());
+
+                System.out.print("Title:");
+                bookReference.setTitle(user_input.next());
+
+                System.out.print("Publisher:");
+                bookReference.setPublisher(user_input.next());
+
+                System.out.print("Year:");
+                bookReference.setYear(user_input.next());
+
+                references.add(bookReference);
+
+                System.out.println("");
+                System.out.println("Entry added!");
+
+                System.out.println("Choose: 1) Add another entry 2) Get all entries in Bibtex-format");
+
+                if(user_input.next().equals("2")) addNewEntry = false;
+            } else {
+                System.out.println("Invalid selection.");
+                System.out.println("");
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Generated BibTeX:");
+    }
+}
