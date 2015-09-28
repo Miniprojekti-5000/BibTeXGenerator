fe563d86854859b2c94c3eda3da8f8ac352aab59
snuarrow <henri.vaara@helsinki.fi>
2015-09-29 01:04:10 +0300

    user interface updated

--- src/main/java/ohtu/miniprojekti5000/ui/UI.java
+++ src/main/java/ohtu/miniprojekti5000/ui/UI.java
@@ -19,6 +19,72 @@ public class UI {
         user_interface_state = 0;
     }
     
+    public void run(int user_interface_state) {
+        Scanner scanner = new Scanner(System.in);
+        
+        switch(user_interface_state)
+        {
+            case 0 : // asks for what to do.
+            {
+                user_interface_state = askWhatToDo(scanner);
+                
+            } break;
+                
+            case 1 : // asks book type inputs
+            {
+                askBookTypeInputs(scanner);
+                user_interface_state = 0;
+            } break;
+            case 2 : // prints bibtex
+            {
+                printBibtexs();
+                user_interface_state = 0;
+            } break;
+        }
+        
+        run(user_interface_state);
+    }
+    private int askWhatToDo(Scanner scanner)
+    {
+        printChoices();
+        int input = selectionInquiry(scanner);
+        while (!inputValidator(input))
+        {
+            printChoices();
+            input = selectionInquiry(scanner);
+        }
+        return input;
+    }
+    
+    private void askBookTypeInputs(Scanner scanner)
+    {
+        System.out.println(show_selection(user_interface_state));
+        String heading = headingInquiry(scanner);
+        String author = authorInquiry(scanner);
+        String title = titleInquiry(scanner);
+        String publisher = publisherInquiry(scanner);
+        String year = yearInquiry(scanner);
+                
+        BookReference bookreference = new BookReference();
+        bookreference.setHeading(heading);
+        bookreference.setAuthor(author);
+        bookreference.setTitle(title);
+        bookreference.setPublisher(publisher);
+        bookreference.setYear(year);
+        references.add(bookreference);
+    }
+    
+    
+    private void printBibtexs()
+    {
+        System.out.println("Generated bibtex");
+        for (ReferenceInterface reference : references.getAll())
+        {
+            System.out.println(reference.toString());
+            System.out.println("");
+        }
+    }
+    
     private void printChoices()
     {
         System.out.println("select one and hit ENTER:\n");
@@ -31,9 +97,6 @@ public class UI {
             System.out.println("2) create bibtex from all entries");
         }
     }
-    
-    
-    
     
     private String show_selection(int selection)
     {
@@ -73,7 +136,6 @@ public class UI {
         System.out.println("Year:");
         return user_input.nextLine();
     }
-    
     private boolean inputValidator(int input)
     {
         switch (input)
@@ -83,129 +145,5 @@ public class UI {
             case 2 : return true;
         }
         return false;
-    }
-    
-
-    public void run(int user_interface_state) {
-        Scanner scanner = new Scanner(System.in);
-        
-        switch(user_interface_state)
-        {
-            case 0 : // asks for what to do.
-            {
-                printChoices();
-                int input = selectionInquiry(scanner);
-                while (!inputValidator(input))
-                {
-                    printChoices();
-                    input = selectionInquiry(scanner);
-                }
-                user_interface_state = input;
-            } break;
-                
-            case 1 : // asks book type inputs
-            {
-                System.out.println(show_selection(user_interface_state));
-                String heading = headingInquiry(scanner);
-                String author = authorInquiry(scanner);
-                String title = titleInquiry(scanner);
-                String publisher = publisherInquiry(scanner);
-                String year = yearInquiry(scanner);
-                
-                BookReference bookreference = new BookReference();
-                bookreference.setHeading(heading);
-                bookreference.setAuthor(author);
-                bookreference.setTitle(title);
-                bookreference.setPublisher(publisher);
-                bookreference.setYear(year);
-                references.add(bookreference);
-                user_interface_state = 0;
-            } break;
-            case 2 : // prints bibtex
-            {
-                System.out.println("Generated bibtex");
-                for (ReferenceInterface reference : references.getAll())
-                {
-                    System.out.println(reference.toString());
-                    System.out.println("");
-                }
-                user_interface_state = 0;
-            } break;
-        }
-        
-        run(user_interface_state);
-        
-        
-        
-        
-//        boolean addNewEntry = true;
-//
-//        Scanner user_input = new Scanner(System.in);
-//
-//        while (addNewEntry) {
-//            System.out.println("What type of reference?");
-//            System.out.println("1) Book");
-//            System.out.println("");
-//
-//            System.out.print("Make your selection and hit ENTER: ");
-//
-//            if (user_input.nextLine().equals("1")) {
-//                BookReference bookReference = new BookReference();
-//
-//                System.out.println("Add new \"Book\" entry");
-//                System.out.println("");
-//
-//                System.out.print("Heading:");
-//                bookReference.setHeading(user_input.nextLine());
-//
-//                System.out.print("Author:");
-//                bookReference.setAuthor(user_input.nextLine());
-//
-//                System.out.print("Title:");
-//                bookReference.setTitle(user_input.nextLine());
-//
-//                System.out.print("Publisher:");
-//                bookReference.setPublisher(user_input.nextLine());
-//
-//                System.out.print("Year:");
-//                bookReference.setYear(user_input.nextLine());
-//
-//                references.add(bookReference);
-//
-//                System.out.println("");
-//                System.out.println("");
-//                System.out.println("Entry added!");
-//
-//                boolean invalidSelection = true;
-//
-//                while(invalidSelection) {
-//                    System.out.println("Choose: 1) Add another entry 2) Get all entries in Bibtex-format");
-//                    System.out.println("");
-//
-//                    System.out.print("Make your selection and hit ENTER: ");
-//
-//                    String input = user_input.next();
-//
-//                    if (input.equals("1")) {
-//                        invalidSelection = false;
-//                    } else if(input.equals("2")) {
-//                        addNewEntry = false;
-//                        invalidSelection = false;
-//                    }
-//                }
-//            } else {
-//                System.out.println("Invalid selection.");
-//                System.out.println("");
-//            }
-//        }
-//
-//        System.out.println("");
-//        System.out.println("Generated BibTeX:");
-//        System.out.println("");
-//
-//        for(ReferenceInterface reference : references.getAll()) {
-//            System.out.println(reference);
-//            System.out.println("");
-//        }
     }
 }
