c66f26b6c10a56886871dd624ed93c2e6630e7fc
matike <matike2@gmail.com>
2015-09-28 22:49:04 +0300

    toString-metodia muutettu

--- src/main/java/ohtu/miniprojekti5000/logic/BookReference.java
+++ src/main/java/ohtu/miniprojekti5000/logic/BookReference.java
@@ -33,37 +33,37 @@ public class BookReference implements ReferenceInterface {
         this.year = year;
     }
 
-    public String get_heading_line() {
-        return "@Book{" + heading + ",";
-    }
-
-    public String get_author_line() {
-        return "author = \"" + author + "\",";
-    }
-
-    public String get_title_line() {
-        return "  title = \"" + title + "\",";
-    }
-
-    public String get_year_line() {
-        return "  year = \"" + year + "\",";
-    }
-
-    public String get_publisher_line() {
-        return "  publisher = \"" + publisher + "\",";
-    }
-
-    public String get_end_line() {
-        return "}";
-    }
+//    public String get_heading_line() {
+//        return "@Book{" + heading + ",";
+//    }
+//
+//    public String get_author_line() {
+//        return "author = \"" + author + "\",";
+//    }
+//
+//    public String get_title_line() {
+//        return "  title = \"" + title + "\",";
+//    }
+//
+//    public String get_year_line() {
+//        return "  year = \"" + year + "\",";
+//    }
+//
+//    public String get_publisher_line() {
+//        return "  publisher = \"" + publisher + "\",";
+//    }
+//
+//    public String get_end_line() {
+//        return "1";
+//    }
 
     public String toString() {
-        return get_heading_line() + "\n"
-                + get_author_line() + "\n"
-                + get_title_line() + "\n"
-                + get_year_line() + "\n"
-                + get_publisher_line() + "\n"
-                + get_end_line();
+        return "@Book{" + heading + ", \n"
+                + "author = \"" + author + "\",\n"
+                + "title = \"" + title + "\",\n"
+                + "year = \"" + year + "\",\n"
+                + "publisher = \"" + publisher + "\",\n"
+                + "}";
     }
 
 }
