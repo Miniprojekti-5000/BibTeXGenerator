2c419c42b72cf767eedbeb2263c239c8d8bc9417
avrj <anraiha@cs.helsinki.fi>
2015-10-17 16:35:04 +0300

    parsiminen regexien avulla

--- src/main/java/ohtu/miniprojekti5000/data_access/FileHandler.java
+++ src/main/java/ohtu/miniprojekti5000/data_access/FileHandler.java
@@ -6,6 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import ohtu.miniprojekti5000.domain.ArticleReference;
 import ohtu.miniprojekti5000.domain.BookReference;
 import ohtu.miniprojekti5000.domain.ReferenceInterface;
@@ -16,6 +19,8 @@ import ohtu.miniprojekti5000.domain.ReferenceInterface;
  */
 public class FileHandler
 {
+    private final Pattern fieldPattern = Pattern.compile("([a-zA-Z]+)(\\s)*=(\\s)*\"(\\s)*([a-zA-Z]+)(\\s)*\"(\\s)*(,?)");
+
     private ArrayList<String> btx;
     
     public boolean loadFile(String path)
@@ -43,51 +48,124 @@ public class FileHandler
         
         for (int i = 0; i < btx.size(); i++)
         {
-            if (btx.get(i).contains("@")) // header line
+            if (btx.get(i).trim().charAt(0) == '@') // header line
             {
-                if (btx.get(i).contains("Book")) // is book type bibtex
+                String[] potentialStr = btx.get(i).substring(1).trim().split("\\{");
+                if (potentialStr[0].toLowerCase().contains("book".toLowerCase())) // is book type bibtex
                 {
                     BookReference br = new BookReference();
-                    String[] temp = btx.get(i).split("\\{");
-                    br.setHeading(temp[1].split(",")[0]);
+                    br.setHeading(potentialStr[1].split(",")[0].trim());
+                    Boolean hasNextField = true;
+                    while(hasNextField) {
+                        Matcher m = fieldPattern.matcher(btx.get(i).trim());
+
+                        // if our pattern matches the string, we can try to extract our groups
+                        if (m.find()) {
+                            // get the two groups we were looking for
+                            String fieldName = m.group(1);
+                            String fieldValue = m.group(5);
+                            String possibleComma = m.group(8);
+
+                            if (fieldName.toLowerCase().equals("author".toLowerCase()))
+                                br.setAuthor(fieldValue);
+                            else if (fieldName.toLowerCase().equals("title".toLowerCase()))
+                                br.setTitle(fieldValue);
+                            else if (fieldName.toLowerCase().equals("publisher".toLowerCase()))
+                                br.setPublisher(fieldValue);
+                            else if (fieldName.toLowerCase().equals("year".toLowerCase()))
+                                br.setYear(fieldValue);
+                            else if (fieldName.toLowerCase().equals("volume".toLowerCase()))
+                                br.setVolume(fieldValue);
+                            else if (fieldName.toLowerCase().equals("address".toLowerCase()))
+                                br.setAddress(fieldValue);
+                            else if (fieldName.toLowerCase().equals("series".toLowerCase()))
+                                br.setSeries(fieldValue);
+                            else if (fieldName.toLowerCase().equals("edition".toLowerCase()))
+                                br.setEdition(fieldValue);
+                            else if (fieldName.toLowerCase().equals("month".toLowerCase()))
+                                br.setMonth(fieldValue);
+                            else if (fieldName.toLowerCase().equals("note".toLowerCase()))
+                                br.setNote(fieldValue);
+                            else if (fieldName.toLowerCase().equals("key".toLowerCase()))
+                                br.setKey(fieldValue);
+
+
+                            if(possibleComma.isEmpty()) hasNextField = false;
+                        }
+                        i++;
+                    }
                     i++;
-                    temp = btx.get(i).split("\"");
-                    br.setAuthor(temp[1]);
+                    /*
+                    br.setAuthor(btx.get(i).split("\"")[1].trim());
                     i++;
-                    temp = btx.get(i).split("\"");
-                    br.setTitle(temp[1]);
+                    br.setTitle(btx.get(i).split("\"")[1].trim());
                     i++;
-                    temp = btx.get(i).split("\"");
-                    br.setYear(temp[1]);
+                    br.setYear(btx.get(i).split("\"")[1].trim());
                     i++;
-                    temp = btx.get(i).split("\"");
-                    br.setPublisher(temp[1]);
-                    
+                    br.setPublisher(btx.get(i).split("\"")[1].trim());
+                    */
                     returnlist.add(br);
                 }
-                else if (btx.get(i).contains("Article"))
+                else if (potentialStr[0].toLowerCase().contains("article".toLowerCase())) // is article type bibtex
                 {
                     ArticleReference ar = new ArticleReference();
-                    String[] temp = btx.get(i).split("\\{");
-                    ar.setHeading(temp[1].split(",")[0]);
+                    ar.setHeading(potentialStr[1].split(",")[0].trim());
                     i++;
-                    ar.setAuthor(btx.get(i).split("\"")[1]);
+                    Boolean hasNextField = true;
+                    while(hasNextField) {
+                        Matcher m = fieldPattern.matcher(btx.get(i).trim());
+
+                        // if our pattern matches the string, we can try to extract our groups
+                        if (m.find()) {
+                            // get the two groups we were looking for
+                            String fieldName = m.group(1);
+                            String fieldValue = m.group(5);
+                            String possibleComma = m.group(8);
+
+                            if (fieldName.toLowerCase().equals("author".toLowerCase()))
+                                ar.setAuthor(fieldValue);
+                            else if (fieldName.toLowerCase().equals("title".toLowerCase()))
+                                ar.setTitle(fieldValue);
+                            else if (fieldName.toLowerCase().equals("journal".toLowerCase()))
+                                ar.setJournal(fieldValue);
+                            else if (fieldName.toLowerCase().equals("year".toLowerCase()))
+                                ar.setYear(fieldValue);
+                            else if (fieldName.toLowerCase().equals("volume".toLowerCase()))
+                                ar.setVolume(fieldValue);
+                            else if (fieldName.toLowerCase().equals("number".toLowerCase()))
+                                ar.setNumber(fieldValue);
+                            else if (fieldName.toLowerCase().equals("pages".toLowerCase()))
+                                ar.setPages(fieldValue);
+                            else if (fieldName.toLowerCase().equals("month".toLowerCase()))
+                                ar.setMonth(fieldValue);
+                            else if (fieldName.toLowerCase().equals("key".toLowerCase()))
+                                ar.setKey(fieldValue);
+                            else if (fieldName.toLowerCase().equals("note".toLowerCase()))
+                                ar.setNote(fieldValue);
+
+                            if(possibleComma.isEmpty()) hasNextField = false;
+                        }
+                        i++;
+                   }
+/*
+                    ar.setAuthor(btx.get(i).split("\"")[1].trim());
                     i++;
-                    ar.setTitle(btx.get(i).split("\"")[1]);
+
+                    ar.setTitle(btx.get(i).split("\"")[1].trim());
                     i++;
-                    ar.setJournal(btx.get(i).split("\"")[1]);
+                    ar.setJournal(btx.get(i).split("\"")[1].trim());
                     i++;
-                    ar.setYear(btx.get(i).split("\"")[1]);
+                    ar.setYear(btx.get(i).split("\"")[1].trim());
                     i++;
-                    ar.setVolume(btx.get(i).split("\"")[1]);
-                    
+                    ar.setVolume(btx.get(i).split("\"")[1].trim());
+                    */
                     returnlist.add(ar);
                 }
             }
         }
         return returnlist;
     }
-    
+
     public boolean appendFile(String path, String reference)
     {
         try
