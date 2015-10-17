8bf413e272d1e00eb651739f4e6915e2532c371f
avrj <anraiha@cs.helsinki.fi>
2015-10-17 16:57:53 +0300

    lisätty inproceedingsin parsiminen

--- src/main/java/ohtu/miniprojekti5000/data_access/FileHandler.java
+++ src/main/java/ohtu/miniprojekti5000/data_access/FileHandler.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 
 import ohtu.miniprojekti5000.domain.ArticleReference;
 import ohtu.miniprojekti5000.domain.BookReference;
+import ohtu.miniprojekti5000.domain.InproceedingsReference;
 import ohtu.miniprojekti5000.domain.ReferenceInterface;
 
 /**
@@ -160,6 +161,67 @@ public class FileHandler
                     ar.setVolume(btx.get(i).split("\"")[1].trim());
                     */
                     returnlist.add(ar);
+                }
+                else if (potentialStr[0].toLowerCase().contains("inproceedings".toLowerCase())) // is article type bibtex
+                {
+                    InproceedingsReference ir = new InproceedingsReference();
+                    ir.setHeading(potentialStr[1].split(",")[0].trim());
+                    i++;
+                    Boolean hasNextField = true;
+                    while(hasNextField) {
+                        Matcher m = fieldPattern.matcher(btx.get(i).trim());
+
+                        // if our pattern matches the string, we can try to extract our groups
+                        if (m.find()) {
+                            // get the two groups we were looking for
+                            String fieldName = m.group(1);
+                            String fieldValue = m.group(5);
+                            String possibleComma = m.group(8);
+
+                            if (fieldName.toLowerCase().equals("author".toLowerCase()))
+                                ir.setAuthor(fieldValue);
+                            else if (fieldName.toLowerCase().equals("title".toLowerCase()))
+                                ir.setTitle(fieldValue);
+                            else if (fieldName.toLowerCase().equals("booktitle".toLowerCase()))
+                                ir.setBookTitle(fieldValue);
+                            else if (fieldName.toLowerCase().equals("year".toLowerCase()))
+                                ir.setYear(fieldValue);
+                            else if (fieldName.toLowerCase().equals("volume".toLowerCase()))
+                                ir.setVolume(fieldValue);
+                            else if (fieldName.toLowerCase().equals("series".toLowerCase()))
+                                ir.setSeries(fieldValue);
+                            else if (fieldName.toLowerCase().equals("address".toLowerCase()))
+                                ir.setAddress(fieldValue);
+                            else if (fieldName.toLowerCase().equals("editor".toLowerCase()))
+                                ir.setEditor(fieldValue);
+                            else if (fieldName.toLowerCase().equals("organization".toLowerCase()))
+                                ir.setOrganization(fieldValue);
+                            else if (fieldName.toLowerCase().equals("publisher".toLowerCase()))
+                                ir.setPublisher(fieldValue);
+                            else if (fieldName.toLowerCase().equals("month".toLowerCase()))
+                                ir.setMonth(fieldValue);
+                            else if (fieldName.toLowerCase().equals("key".toLowerCase()))
+                                ir.setKey(fieldValue);
+                            else if (fieldName.toLowerCase().equals("note".toLowerCase()))
+                                ir.setNote(fieldValue);
+
+                            if(possibleComma.isEmpty()) hasNextField = false;
+                        }
+                        i++;
+                    }
+/*
+                    ar.setAuthor(btx.get(i).split("\"")[1].trim());
+                    i++;
+
+                    ar.setTitle(btx.get(i).split("\"")[1].trim());
+                    i++;
+                    ar.setJournal(btx.get(i).split("\"")[1].trim());
+                    i++;
+                    ar.setYear(btx.get(i).split("\"")[1].trim());
+                    i++;
+                    ar.setVolume(btx.get(i).split("\"")[1].trim());
+                    */
+                    returnlist.add(ir);
                 }
             }
         }
