1c193d0a8faaf5c35644678c2b893f9fd3e471f9
matike <matike2@gmail.com>
2015-10-19 05:24:26 +0300

    Testit ArticleReferencen vapaaehtoisille kentille

--- src/test/java/ohtu/miniprojekti5000/domain/ArticleReferenceTest.java
+++ src/test/java/ohtu/miniprojekti5000/domain/ArticleReferenceTest.java
@@ -6,101 +6,171 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ArticleReferenceTest {
+
     private ArticleReference reference;
+    private SpecialCharConverter converter;
 
     @Before
     public void setUp() {
-        reference = new ArticleReference("heading", "author", "title", "journal", "year", "volume");
+        reference = new ArticleReference();
+        converter = new SpecialCharConverter();
     }
 
     @Test
     public void headingIsSet() {
-        String heading = "newHeading";
+        String heading = "heading";
 
         reference.setHeading(heading);
-        assertEquals(heading, reference.getHeading());
+        assertEquals(heading, reference.heading);
     }
 
     @Test
     public void headingIsGet() {
         String heading = "heading";
 
+        reference.setHeading(heading);
         assertEquals(heading, reference.getHeading());
     }
 
     @Test
     public void authorIsSet() {
-        String author = "newAuthor";
+        String author = "author";
 
         reference.setAuthor(author);
-        assertEquals(author, reference.getAuthor());
+        assertEquals(author, reference.required_fields.get("author"));
     }
 
     @Test
     public void authorIsGet() {
         String author = "author";
 
+        reference.setAuthor(author);
         assertEquals(author, reference.getAuthor());
     }
 
     @Test
     public void titleIsSet() {
-        String title = "newTitle";
+        String title = "title";
 
         reference.setTitle(title);
-        assertEquals(title, reference.getTitle());
+        assertEquals(title, reference.required_fields.get("title"));
     }
 
     @Test
     public void titleIsGet() {
         String title = "title";
 
+        reference.setTitle(title);
         assertEquals(title, reference.getTitle());
     }
 
     @Test
     public void JournalIsSet() {
-        String journal = "newJournal";
+        String journal = "journal";
 
         reference.setJournal(journal);
-        assertEquals(journal, reference.getJournal());
+        assertEquals(journal, reference.required_fields.get("journal"));
     }
 
     @Test
     public void journalIsGet() {
         String journal = "journal";
 
+        reference.setJournal(journal);
         assertEquals(journal, reference.getJournal());
     }
 
     @Test
     public void yearIsSet() {
-        String year = "newYear";
+        String year = "year";
 
         reference.setYear(year);
-        assertEquals(year, reference.getYear());
+        assertEquals(year, reference.required_fields.get("year"));
     }
 
     @Test
     public void yearIsGet() {
         String year = "year";
 
+        reference.setYear(year);
         assertEquals(year, reference.getYear());
     }
 
     @Test
     public void volumeIsSet() {
-        String volume = "newVolume";
+        String volume = "volume";
 
         reference.setVolume(volume);
-        assertEquals(volume, reference.getVolume());
+        assertEquals(volume, reference.required_fields.get("volume"));
     }
 
     @Test
     public void volumeIsGet() {
         String volume = "volume";
 
+        reference.setVolume(volume);
         assertEquals(volume, reference.getVolume());
+    }
+
+    @Test
+    public void numberIsSet() {
+        String number = "number";
+
+        reference.setNumber(number);
+        assertEquals(number, reference.optional_fields.get("number"));
+    }
+
+    @Test
+    public void pageIsSet() {
+        String pages = "pages";
+
+        reference.setPages(pages);
+        assertEquals(pages, reference.optional_fields.get("pages"));
+    }
+
+    @Test
+    public void monthIsSet() {
+        String month = "month";
+
+        reference.setMonth(month);
+        assertEquals(month, reference.optional_fields.get("month"));
+    }
+
+    @Test
+    public void noteIsSet() {
+        String note = "note";
+
+        reference.setNote(note);
+        assertEquals(note, reference.optional_fields.get("note"));
+    }
+
+    @Test
+    public void keyIsSet() {
+        String key = "key";
+
+        reference.setKey(key);
+        assertEquals(key, reference.optional_fields.get("key"));
+    }
+    
+    @Test
+    public void optionalFieldisSet() {
+        
+    }
+
+    @Test
+    public void optionalFieldGetReturnCorrectOutput() {
+        String field = "field";
+
+        assertEquals("     " + field + " = \"" + field + "\"\n", reference.getOptionalField(converter, field, field, Boolean.FALSE));
+
+    }
+
+    @Test
+    public void requiredFieldGetReturnCorrectOutput() {
+        String field = "field";
+
+        assertEquals("     " + field + " = \"" + field + "\"\n", reference.getRequiredField(converter, field, field, Boolean.FALSE));
+
     }
 
     @Test
